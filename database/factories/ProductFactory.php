dc67751a9e10da47b0d1d674a29a3056190930fc
Maikel Dekker <99040039@mydavinci.nl>
2019-01-25 14:25:58 +0100

    fix

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,10 +4,6 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Product::class, function (Faker $faker) {
     return [
-        'title' => $faker->word,
-        'description' => $faker->sentence,
-        'catagory' => $faker->word,
-        'price' => $faker->randomDigit,
-        'amount' => 1,
+        //
     ];
 });
